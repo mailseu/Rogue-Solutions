a774d4da1eb796984c326e139e43d5ed1dd3d7df
Nicholai <nicholaiagdeppa@gmail.com>
2026-02-28 17:53:06 -0800

    test commiting remotely

--- src/main.ino
+++ src/main.ino
@@ -2,9 +2,14 @@
 // Blink example
 
 void setup() {
-
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(50); // wait for a second
+    digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+    delay(50); // wait for a second
 }
+
+// This code will blink the built-in LED on and off every 50 milliseconds.
