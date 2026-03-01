c425767d3048b07724f8980252a226a66cbcb349
Henry <hsamala4709@gmail.com>
2026-02-28 17:39:49 -0800

    delete blink code

--- src/main.ino
+++ src/main.ino
@@ -2,13 +2,9 @@
 // Blink example
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  // Set built-in LED pin as output
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn LED on
-  delay(1000);                      // Wait 1 second (1000 ms)
-  
-  digitalWrite(LED_BUILTIN, LOW);   // Turn LED off
-  delay(1000);                      // Wait 1 second
+
 }
