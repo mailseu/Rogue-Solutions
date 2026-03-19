dd01d6e354321738d9afe5a6b860f8bd30fd0c78
henry <hsamala4709@gmail.com>
2026-03-19 13:55:44 -0700

    motor test

--- src/main.ino
+++ src/main.ino
@@ -1,11 +1,57 @@
 #include <Arduino.h>
-#include "blink_esp32.h"
-#define LED 2
+
+const int RPWM = 5;
+const int LPWM = 18;
+
+const int chRPWM = 0;
+const int chLPWM = 1;
+
+const int pwmFreq = 1000;
+const int pwmResolution = 8;
+
+void motorForward(int speedVal);
+void motorReverse(int speedVal);
+void motorStop();
 
 void setup() {
-    pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+
+  // Force pins low first as plain GPIO
+  pinMode(RPWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
+  digitalWrite(RPWM, LOW);
+  digitalWrite(LPWM, LOW);
+
+  delay(200);
+
+  // Setup PWM
+  ledcSetup(chRPWM, pwmFreq, pwmResolution);
+  ledcSetup(chLPWM, pwmFreq, pwmResolution);
+
+  ledcAttachPin(RPWM, chRPWM);
+  ledcAttachPin(LPWM, chLPWM);
+
+  motorStop();
 }
 
 void loop() {
-    blinkOnce_esp32(LED);
+  motorStop();
+  delay(1000);
+}
+
+void motorForward(int speedVal) {
+  speedVal = constrain(speedVal, 0, 255);
+  ledcWrite(chLPWM, 0);
+  ledcWrite(chRPWM, speedVal);
+}
+
+void motorReverse(int speedVal) {
+  speedVal = constrain(speedVal, 0, 255);
+  ledcWrite(chRPWM, 0);
+  ledcWrite(chLPWM, speedVal);
+}
+
+void motorStop() {
+  ledcWrite(chRPWM, 0);
+  ledcWrite(chLPWM, 0);
 }
